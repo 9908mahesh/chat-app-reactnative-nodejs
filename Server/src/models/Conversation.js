f5ae14dd02f3edc8760bff9c08b9732f0bd14307
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 08:15:19 +0530

    Update Conversation.js

--- Server/src/models/Conversation.js
+++ Server/src/models/Conversation.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-
 const ConversationSchema = new mongoose.Schema({
-participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-lastMessage: { text: String, senderId: mongoose.Schema.Types.ObjectId, createdAt: Date },
-updatedAt: { type: Date, default: Date.now }
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  lastMessage: { text: String, senderId: mongoose.Schema.Types.ObjectId, createdAt: Date },
+  updatedAt: { type: Date, default: Date.now }
 });
 
-
 module.exports = mongoose.model('Conversation', ConversationSchema);
+
