e52ff5ae6377c5dc181135dfc1b4fdbe4db74cf1
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 08:45:58 +0530

    Update api.js

--- mobile/src/api.js
+++ mobile/src/api.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Replace with your server host (e.g. http://192.168.1.10:4000 or https://your-deploy-url)
-const API_URL = 'http://<YOUR_SERVER_HOST>:4000';
+//const API_URL = 'http://<YOUR_SERVER_HOST>:4000';
+const API_URL = "https://your-app.onrender.com";
+
 
 const api = axios.create({ baseURL: API_URL });
 
