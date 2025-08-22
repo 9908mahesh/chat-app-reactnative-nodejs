6ef2007e0b02356b823d04d56552fa418c82d7bf
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 08:16:19 +0530

    Update auth.js

--- Server/src/middleware/auth.js
+++ Server/src/middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-
 module.exports = async function (req, res, next) {
-const auth = req.headers.authorization;
-if (!auth) return res.status(401).json({ error: 'missing token' });
-const token = auth.split(' ')[1];
-try {
-const payload = jwt.verify(token, process.env.JWT_SECRET);
-const user = await User.findById(payload.id).select('-passwordHash');
-if (!user) return res.status(401).json({ error: 'user not found' });
-req.user = user;
-next();
-} catch (e) {
-return res.status(401).json({ error: 'invalid token' });
-}
+  const auth = req.headers.authorization;
+  if (!auth) return res.status(401).json({ error: 'missing token' });
+  const token = auth.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(payload.id).select('-passwordHash');
+    if (!user) return res.status(401).json({ error: 'user not found' });
+    req.user = user;
+    next();
+  } catch (e) {
+    return res.status(401).json({ error: 'invalid token' });
+  }
 };
+
