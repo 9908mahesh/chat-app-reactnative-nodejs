33aeb55866b1909702d7c4e75548a3526315b806
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 08:20:07 +0530

    Create index.js

---
+++ Server/src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+const { connect } = require('./db');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+const convRoutes = require('./routes/conversations');
+const { setupSocket } = require('./socket');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/conversations', convRoutes);
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+setupSocket(io);
+
+const PORT = process.env.PORT || 4000;
+connect(process.env.MONGO_URI).then(() => {
+  server.listen(PORT, () => console.log('Server listening on', PORT));
+}).catch((err) => {
+  console.error('DB connection failed', err);
+});
