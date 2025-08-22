0f4aeb32644506dbf762e34c4ee322d2dcf84f03
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 08:26:38 +0530

    Create api.js

---
+++ mobile/src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Replace with your server host (e.g. http://192.168.1.10:4000 or https://your-deploy-url)
+const API_URL = 'http://<YOUR_SERVER_HOST>:4000';
+
+const api = axios.create({ baseURL: API_URL });
+
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
