658b92b019087d78355965b4a5a8dfdb2998ea4c
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 08:18:33 +0530

    Update users.js

--- Server/src/routes/users.js
+++ Server/src/routes/users.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const User = require('../models/User');
 
-
 router.get('/', auth, async (req, res) => {
-const users = await User.find({ _id: { $ne: req.user._id } }).select('-passwordHash');
-res.json(users);
+  const users = await User.find({ _id: { $ne: req.user._id } }).select('-passwordHash');
+  res.json(users);
 });
-
 
 module.exports = router;
