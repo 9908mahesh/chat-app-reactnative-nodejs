13a96ffbe4c567c191d585e63ed432b2c8d655ed
9908mahesh <91625951+9908mahesh@users.noreply.github.com>
2025-08-22 07:57:59 +0530

    Create db.js

---
+++ Server/src/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+async function connect(uri) {
+await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log('MongoDB connected');
+}
+
+
+module.exports = { connect };
